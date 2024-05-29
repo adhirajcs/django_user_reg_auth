cb97fcfb9fb529ab9b981b432a0850669f3d3f44
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-05-29 11:13:25 +0000

    Added register and Login functionality

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .forms import CreateUserForm, LoginForm
+
+# Authentication Models and methods
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 
 def homepage(request):
@@ -7,11 +11,42 @@ def homepage(request):
 
 
 def register(request):
-	return render(request, 'myapp/register.html')
+
+	form = CreateUserForm()
+
+	if request.method == "POST":
+		form = CreateUserForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+
+			return redirect("login")
+		
+	context = {'registerform': form}
+
+	return render(request, 'myapp/register.html', context=context)
 
 
 def my_login(request):
-	return render(request, 'myapp/my-login.html')
+
+	form = LoginForm()
+
+	if request.method == "POST":
+
+		form = LoginForm(request, data=request.POST)
+
+		if form.is_valid():
+			username =  request.POST.get('username')
+			password =  request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				auth.login(request, user)
+				return redirect("dashboard")
+	
+	context = {'loginform': form}
+
+	return render(request, 'myapp/my-login.html', context=context)
 
 
 def dashboard(request):
